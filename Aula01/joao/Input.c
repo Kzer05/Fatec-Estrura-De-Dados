56399f919051cdfe577218a1e41f0f84509304ad
joaomagi <joaofrancisco.leaomagi@outlook.com>
2025-02-17 20:23:50 -0300

    deleted:    Aula01/joao.exe	deleted:    Aula01/joao.exe 	renamed:    Aula01/joao.c -> Aula01/joao/Helloworld.c 	new file:   Aula01/joao/Input.c

---
+++ Aula01/joao/Input.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int numero;
+
+    printf("Digite um numero interio por favor \n");
+    printf("INTEIRO PELA AMOR DE DEUS\n");
+    scanf("%d", &numero);
+
+    printf("Parabens voce digitou um numero inteiro, Obrigado(kissemoji) \n");
+    printf("O numero digitado %d \n",numero);
+
+    return 0;
+
+}
