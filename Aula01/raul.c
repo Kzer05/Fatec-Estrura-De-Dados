43711d5366da3338d5ee50dd67bf16b5fbca0348
RickD248 <raul11.serrano@gmail.com>
2025-02-17 21:32:13 -0300

    modified:   Aula01/raul.c 	modified:   Aula01/raul.exe

--- Aula01/raul.c
+++ Aula01/raul.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
     char mundo[50];
+    int numerointeirointeressante;
+    float numerorealaindamaisinteressante;
+    double numerorealaindamaisexpecificoeinteressante;
+
     printf("Olá ???\n");
     scanf("%[^\n]", mundo);
-    printf("Olá %s", mundo);
+    getchar();
+    printf("O número ??? é o mais interessante");
+    scanf("%numerointeirointeressante", &numerointeirointeressante);
+    printf("Seguido do número real ???");
+    scanf("%numerorealaindamaisinteressante", &numerorealaindamaisinteressante);
+    printf("E do número ???, que é mais expecifico ainda");
+    scanf("%numerorealaindamaisexpecificoeinteressante", &numerorealaindamaisexpecificoeinteressante);
+    printf("Usuário %s\t|NI %d\t| NRI %f\t| NAMIR %f\t", mundo, numerointeirointeressante, numerorealaindamaisinteressante, numerorealaindamaisexpecificoeinteressante);
     return 0;
 }
