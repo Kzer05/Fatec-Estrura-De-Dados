cb231144c038e696d50cb7fc4910b1c0e05668a8
kzer05 <kevingabriel6000@gmail.com>
2025-02-17 21:42:21 -0300

    new file:   Aula01/kevin/a.exe 	new file:   Aula01/kevin/ex01.c 	new file:   Aula01/kevin/ex01.exe 	new file:   Aula01/kevin/ex02.c 	new file:   Aula01/kevin/ex02.exe 	modified:   Aula01/kevin/kevin.c

---
+++ Aula01/kevin/ex01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){ 
+    int qtde_pizzas = 1;
+    float raio = 5.23;
+    char sabor = 'C';
+
+    printf("Voce tem %d pizzas, com raio %f, do sabor tipo %c alabresa", qtde_pizzas, raio, sabor );
+    return 0;
+}
