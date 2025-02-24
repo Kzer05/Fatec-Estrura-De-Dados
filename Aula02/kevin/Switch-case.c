bb85ce4e5f3dfae0eb6eac516bc04b418580bdf0
kzer05 <kevingabriel6000@gmail.com>
2025-02-24 19:53:53 -0300

    deleted:    Aula02/kevin/IfElse.c 	new file:   Aula02/kevin/Switch-case.c 	new file:   Aula02/kevin/maior_idade.c 	new file:   Aula02/kevin/maior_idade.exe

---
+++ Aula02/kevin/Switch-case.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int opcao;
+
+    printf("1. Imprimir");
+    printf("2. Salvar");
+    printf("3. Abrir");
+    printf("4. Sait");
+    
+    scanf("%d", &opcao);
+
+    switch (opcao)
+    {
+    case 1:
+        
+        break;
+    case 2:
+
+        break;
+    case 3:
+
+        break;
+    case 4:
+
+        break;
+
+    default:
+        break;
+    }
+}
